e282e44f3cc75a51c52b951e23de9c90ea4595e3
Leandro Santana <leo93santana@gmail.com>
2020-11-25 05:33:02 -0300

    :dizzy: added parallax effect on event details screen

--- app/src/main/kotlin/com/santana/eventsagenda/ui/eventdetails/EventDetailsActivity.kt
+++ app/src/main/kotlin/com/santana/eventsagenda/ui/eventdetails/EventDetailsActivity.kt
@@ -1,23 +1,28 @@
 package com.santana.eventsagenda.ui.eventdetails
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.santana.eventsagenda.databinding.ActivityDetailsBinding
 import com.santana.eventsagenda.R
+import com.santana.eventsagenda.databinding.ActivityDetailsBinding
 import com.santana.eventsagenda.domain.model.CheckinBO
 import com.santana.eventsagenda.domain.model.EventBO
 import com.santana.eventsagenda.state.EventResponse
-import com.santana.eventsagenda.utils.setVisibility
 import com.santana.eventsagenda.ui.EventsRouter.EVENT_SELECTED_ID
+import com.santana.eventsagenda.utils.formatMonetary
+import com.santana.eventsagenda.utils.toDayMonthYear
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.content_event_description.*
+import kotlinx.android.synthetic.main.content_event_location.*
+import kotlinx.android.synthetic.main.content_event_price.*
+import java.text.NumberFormat
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -42,7 +47,14 @@ class EventDetailsActivity : AppCompatActivity() {
 
     private fun setupViews() {
         showLoading()
+        setupToolbar()
         openCheckinDialog()
+    }
+
+    private fun setupToolbar() {
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 
     private fun openCheckinDialog() {
@@ -53,10 +65,10 @@ class EventDetailsActivity : AppCompatActivity() {
             val btnConfirm = view.findViewById<Button>(R.id.btnConfirmCheckin)
             dialogCheckin = Dialog(this)
             dialogCheckin.setContentView(view)
-            btnConfirm.setOnClickListener{
-                if (etName.text.isNotEmpty() && etEmail.text.isNotEmpty()){
+            btnConfirm.setOnClickListener {
+                if (etName.text.isNotEmpty() && etEmail.text.isNotEmpty()) {
                     viewModel.checkinEvent(etName?.text.toString(), etEmail?.text.toString())
-                }else{
+                } else {
                     if (etName.text.isEmpty()) etName.error = getString(R.string.fill_field)
                     if (etEmail.text.isEmpty()) etEmail.error = getString(R.string.fill_field)
                 }
@@ -66,7 +78,7 @@ class EventDetailsActivity : AppCompatActivity() {
     }
 
     private fun showLoading() {
-        if (dialogLoading==null || !dialogLoading!!.isShowing) {
+        if (dialogLoading == null || !dialogLoading!!.isShowing) {
             dialogLoading = Dialog(this)
             (dialogLoading as Dialog).setContentView(R.layout.dialog_loading)
             (dialogLoading as Dialog).setCancelable(false)
@@ -75,7 +87,7 @@ class EventDetailsActivity : AppCompatActivity() {
     }
 
     private fun hideLoading() {
-        Timer().schedule(1000){
+        Timer().schedule(1000) {
             dialogLoading?.dismiss()
         }
     }
@@ -136,8 +148,22 @@ class EventDetailsActivity : AppCompatActivity() {
         Toast.makeText(this, R.string.checkin_done, Toast.LENGTH_LONG).show()
     }
 
-    private fun populateEventDetails(eventDetails: EventBO) {
-        //TODO fill fields with event data
+    private fun populateEventDetails(event: EventBO) {
+        binding.let {
+            Picasso.get()
+                .load(event.imageUrl)
+                .fit()
+                .placeholder(R.drawable.bg_events)
+                .error(R.drawable.bg_events)
+                .into(ivImage)
+            toolbarLayout.title = event.title
+            toolbar.title = event.title
+            tvPrice.text = event.price.formatMonetary()
+            tvDate.text = event.date.toDayMonthYear()
+            tvLatitude.text = getString(R.string.location_latitude, event.latitude)
+            tvLongitude.text = getString(R.string.location_longitude, event.longitude)
+            tvDescription.text = event.description
+        }
     }
 
     private fun showErrorMessage() {
